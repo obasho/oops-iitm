ee4dc60f7f7fff37119bb18ea5836b92366ed539
obasho <46088709+obasho@users.noreply.github.com>
2023-09-23 20:34:55 +0530

    ,

--- functions/sum of digit.cpp
+++ functions/sum of digit.cpp
@@ -1,5 +1,22 @@
 #include<iostream>
 using namespace std;
-int sumofd
-factorial
-largest divisor
+int sod(int i)
+{
+    int j=i/10;
+    int l=i%10;
+    if(l!=0)
+    {
+        int k=l+sod(j);
+        cout<<k<<"/t"<<j<<endl;
+        return k;
+    }
+    else
+        return 0;
+}
+int main()
+{
+    int r=0;
+    cin>>r;
+    int h=sod(r);
+    cout<<h;
+}
